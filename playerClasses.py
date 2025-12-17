b39df0e04a7f375bb627548a042916f025fc05ec
Spade1095 <henrym181@nycstudents.net>
2025-12-17 10:17:25 -0500

    Create playerClasses.py
    made the player class

---
+++ playerClasses.py
@@ -0,0 +1,32 @@
+class player:
+    def __init__(self,name,HP,speed,attack,defense,evasiveness,inventory,equipped,moves):
+        self.HP = HP
+        self.speed = speed
+        self.attack = attack
+        self.defense = defense
+        self.evasiveness = evasiveness
+        self.inventory = inventory
+        self.equippted = equipped
+        self.moves = moves
+        self.name = name
+    def returnStats(self):
+        stats = {
+            "attack":self.attack,
+            "defense":self.defense,
+            "speed":self.speed,
+            "evasiveness": self.evasiveness
+        }
+        return stats
+# class playerClasses:
+#     def __init__(self,HP,speed,attack,defense,evasiveness,inventory,equipped,moves):
+#         self.HP = HP
+#         self.speed = speed
+#         self.attack = attack
+#         self.defense = defense
+#         self.evasiveness = evasiveness
+#         self.inventory = inventory
+#         self.equippted = equipped
+#         self.moves = moves
+name = input("What is your name?")
+chosenClass = input("What class are you?")
+    
