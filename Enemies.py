0ab0565be47174c6c53851d406983618a1410a5b
Andy Weng <andyweng257@gmail.com>
2025-12-18 10:17:07 -0500

    Update Enemies.py

--- Enemies.py
+++ Enemies.py
@@ -1,17 +1,30 @@
+import random
 
-
+debuff = [
+    {"debuff":"light", "multiplyer":0.8}
+    ]
 
 
 
 class Undead:
-    def __init__(self,debuffs):
-        self.debuffs = debuffs
-        print(debuffs)
+    debuffactive = True
+    if debuffactive == False:
+        debuffmulti = 1
+    else:
+        debuffmulti = debuff[0]["multiplyer"]
 
 class Zombie(Undead):
+    def __init__(self):
+        self.level = random.randint(1, 6)
+        self.name = "Young Zombie"
+        self.health = int(100 * (self.level * .40))
+        self.attack = 4 + self.level
+        self.speed = self.level
+        self.defense = int(3 * self.level * self.debuffmulti)
 
-    def __init__(self, debuffs):
-        super.__init__(self, debuffs)
-
-
-print(Undead("Bob", "Fire"))
+    def display(self):
+        print(self.health)
+        print(self.defense)
+       
+zombie = Zombie()
+zombie.display()
