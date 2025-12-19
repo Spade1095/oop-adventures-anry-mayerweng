4113998734e2bac174c1f21f0162157c3894c6c8
Spade1095 <henrym181@nycstudents.net>
2025-12-19 09:48:20 -0500

    qwef

--- playerClasses.py
+++ playerClasses.py
@@ -1,4 +1,41 @@
-
+# import json
+# data = open("./classes.json",encoding="utf8")
+# classes = json.load(data)
+classes = [   
+        {
+        "name":"Melee",
+        "HP":80,
+        "speed":80,
+        "attack":120,
+        "defense":80,
+        "evasiveness":0.99,
+        "inventory": [],
+        "equipped": ["Basic Sword"],
+        "moves": ["Sword Slash"]
+    },
+        {
+        "name":"Tank",
+        "HP":120,
+        "speed":60,
+        "attack":100,
+        "defense":120,
+        "evasiveness":1,
+        "inventory": [],
+        "equipped": ["Giant Fists","Basic Armor"],
+        "moves": ["Giant Punch"]
+    },
+        {
+        "name":"Assasin",
+        "HP":60,
+        "speed":120,
+        "attack":110,
+        "defense":60,
+        "evasiveness":0.85,
+        "inventory": [],
+        "equipped": ["Basic Dagger"],
+        "moves": ["Stab"]
+    }
+]
 class character:
     def __init__(self,name,HP,speed,attack,defense,evasiveness,inventory,equipped,moves):
         self.HP = HP
@@ -19,17 +56,16 @@ class character:
         }
         return stats
 classNames = []
-for i in range(len(classesjson)):
-    classNames.append(classesjson[i]["name"])
+for i in range(len(classes)):
+    classNames.append(classes[i]["name"])
 
 name = input("What is your name?")
 chosenClass = input(f"What class are you?{classNames}")
-for i in range(len(classesjson)):
-    for i in range*(len(classesjson)):
-        if classesjson[i]["name"] == chosenClass:
-            chosenData = classesjson[i]
+for i in range(len(classes)):
+    for i in range(len(classes)):
+        if classes[i]["name"] == chosenClass:
+            chosenData = classes[i]
             player = character(name,chosenData["HP"],chosenData["speed"],chosenData["attack"],chosenData["defense"],chosenData["evasiveness"],chosenData["inventory"],chosenData["moves"])
 
 
 
-    
