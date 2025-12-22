06fd3c560e25fbc551f40fd130d41225639a62b0
Spade1095 <henrym181@nycstudents.net>
2025-12-22 09:48:32 -0500

    Update playerClasses.py

--- playerClasses.py
+++ playerClasses.py
@@ -5,6 +5,7 @@ classes = [
         {
         "name":"Melee",
         "HP":80,
+        "maxHP":80,
         "speed":80,
         "attack":120,
         "defense":80,
@@ -16,6 +17,7 @@ classes = [
         {
         "name":"Tank",
         "HP":120,
+        "maxHP":120,
         "speed":60,
         "attack":100,
         "defense":120,
@@ -27,6 +29,7 @@ classes = [
         {
         "name":"Assasin",
         "HP":60,
+        "maxHP":60,
         "speed":120,
         "attack":110,
         "defense":60,
@@ -37,8 +40,9 @@ classes = [
     }
 ]
 class character:
-    def __init__(self,name,HP,speed,attack,defense,evasiveness,inventory,equipped,moves):
+    def __init__(self,name,HP,maxHP,speed,attack,defense,evasiveness,inventory,equipped,moves):
         self.HP = HP
+        self.maxHP = maxHP
         self.speed = speed
         self.attack = attack
         self.defense = defense
@@ -59,6 +63,19 @@ class character:
         return self.inventory
     def returnEquipped(self):
         return self.equippted
+    def addToInventory(self,item):
+        self.inventory.append(item)
+    def doDmg(self,dmg):
+        self.HP = self.HP - dmg
+    def heal(self,heal):
+        self.HP = self.HP + heal
+        if self.HP > self.maxHP:
+            self.HP = self.maxHP
+    def isAlive(self):
+        if self.HP > 0:
+            return True
+        else:
+            return False
 classNames = []
 for i in range(len(classes)):
     classNames.append(classes[i]["name"])
