678f771c49970a9a94402f0b40c63efc8f64756f
Spade1095 <henrym181@nycstudents.net>
2025-12-23 09:55:13 -0500

    23fr

--- combat.py
+++ combat.py
@@ -1,5 +1,6 @@
-# from Enemies import mob 
+from Enemies import mob 
 from playerClasses import character
+import random as rand
 moves = [
     {
         "name":"Bite",
@@ -15,4 +16,24 @@ moves = [
     },
 ]
 def battle(enemie,player):
-    enemie
+    enemieStats = enemie.returnStats
+    playerStats = player.returnStats
+    enemieMoves = enemie.returnMoves
+    playerMoves = player.returnMoves
+    battle = True
+    while battle:
+        if playerStats["speed"] > enemieStats["speed"]:
+            for i in range(len(playerMoves)):
+                print(f"{i+1}: {playerMoves[i]}")
+            chosenMove = int(input("Chose a move!"))
+            chosenMove = playerMoves[chosenMove - 1]
+            for i in range(len(moves)):
+                if moves[i]["name"] == chosenMove:
+                    move = moves[i]
+            chanceToHit = move["accuracy"] * enemieStats["evasiveness"] * 100
+            dmg = move["dmg"] * (playerStats[""])
+            if rand.randint(1,100) < chanceToHit:
+                enemie.doDamage()
+
+        
+
