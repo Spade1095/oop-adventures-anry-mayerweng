98f08bd43d66fb49a767b4381c81652f5db1aaa3
Andy Weng <andyweng257@gmail.com>
2026-01-05 10:17:09 -0500

    Update crafting.py

--- crafting.py
+++ crafting.py
@@ -13,14 +13,39 @@ crafting_recipes = [
         ]
 
 
+class craftingfunc():
+    def __init__(self, item_input):
 
-def checkcraft():
-       can_craft = None
-       for item in player_invetory:
-            if item in player_invetory:
-                can_craft = True
-            else:
-                can_craft = False
-                  
-                  
-checkcraft()
+        self.chosen_item = None
+
+
+
+        self.recipes =[
+    
+        {"item":"Iron Sword",
+          "recipe":"Iron "*2},
+        {"item":"Blood Wand", 
+         "recipe": "Vampire Essense "*5},
+        {"item":"Blood Sword", 
+         "recipe":"Vampire Essense, Vampire Essense, Vampire Essense, Vampire Essense, Vampire Essense, Iron, Iron"},
+        {"item":"Vampire Mask",
+         "recipe":"Vampire Essence, Vampire Essence, Bones, Bones, Bones"}
+        ]
+
+
+    def inputmats(self)
+
+    def checkcraft(self, desired_item):
+        for recipes in self.recipes:
+            if self.recipes["item"].lower() == desired_item.lower():
+                self.chosen_item = recipes["item"]
+            if self.chosen_item == None:
+                return
+            
+
+
+
+
+
+           
+
