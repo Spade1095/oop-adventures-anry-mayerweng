5be9e0c8ee0fedf78f5c99084ef3c81c521574d0
Spade1095 <henrym181@nycstudents.net>
2026-01-14 10:00:32 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,120 @@
+import random as r
+import combat as combat
+import Enemies as enemies
+import playerClasses as classes
+import 
+classes = [   
+        {
+        "name":"Melee",
+        "HP":80,
+        "maxHP":80,
+        "speed":80,
+        "attack":120,
+        "defense":80,
+        "evasiveness":0.99,
+        "inventory": [],
+        "equipped": ["Basic Sword"],
+        "moves": ["Sword Slash"]
+    },
+        {
+        "name":"Tank",
+        "HP":120,
+        "maxHP":120,
+        "speed":60,
+        "attack":100,
+        "defense":120,
+        "evasiveness":1,
+        "inventory": [],
+        "equipped": ["Giant Fists","Basic Armor"],
+        "moves": ["Giant Punch"]
+    },
+        {
+        "name":"Assasin",
+        "HP":60,
+        "maxHP":60,
+        "speed":120,
+        "attack":110,
+        "defense":60,
+        "evasiveness":0.85,
+        "inventory": [],
+        "equipped": ["Basic Dagger"],
+        "moves": ["Stab"]
+    }
+]
+stepsLeft = 7
+bosses = [
+    {
+        "name":"Giant Zombie",
+        "text":"You hear growling coming from door ",
+        "type":"Encounter",
+        "mob":"Giant Zombie"
+     },
+     {
+         "name":"Damien",
+         "Text":"You hear absolutly nothing from door ",
+         "type":"Other",
+         "mob":None
+     }
+]
+rooms = [
+    {
+        "name":"Zombie Room",
+        "encounter":"Zombie",
+        "reward":["coins","Leather Chestplate","Leather Leggings","Leather Boots","Health Charm"]
+    },
+    {
+        "name":"Vampire Room",
+        "encounter":"Vampire",
+        "reward":["coins","Leather Chestplate","Iron Leggings","Iron Boots","Speed Charm"]
+    },
+    {
+        "name":"Skeleton Room",
+        "enounter":"Skeleton",
+        "reward":["coins","Leather Chestplate","Leather Leggings","Leather Boots","Damage Charm"]
+    }
+]
+mobs = [
+    {
+        "name": "Zombie",
+        "hp":r.randint(50,70), 
+        "attack":r.randint(4,7), 
+        "mob_name": r.choice([
+        "Shambling Corpse","Rotwalker","Graveborn","Fleshdragger","Mudclaw","Hollow One","Graveshuffler","Rotting Husk","Bonechewer","Ashen Dead"]), 
+        "lootdrops":r.choice(["Flesh","Iron","Zombie Head", "Leather Chestplate"]), 
+        "speed":r.randint(1,2), 
+        "defense":r.randint(2,6),
+        "evasiveness":1,
+        "chance":r.randint(1,4),
+        "debuff": None,
+        "debuffmulti": 1,
+        "moves":["Bite","Bad Breath"]
+    },
+    {
+        "name": "Vampire",
+        "hp":r.randint(60,100), 
+        "attack":r.randint(3,6), 
+        "mob_name": r.choice(["Draven", "Lilith", "Lucien", "Nyx", "Vesper"]), 
+        "lootdrops":r.choice(["Vampire Essence","Vampire Essence","Silver helment", "Silver Leggings", "Silver Chestplate"]), 
+        "speed":r.randint(4,7), 
+        "defense":r.randint(1,4),
+        "evasiveness":0.9,
+        "chance":r.randint(1,6),
+        "debuff": None,
+        "debuffmulti": 1,
+        "moves":["Bite","Blood Shot"]
+    },
+    {
+    "name": "Skeleton",
+        "hp":r.randint(30,50), 
+        "attack":r.randint(5,9), 
+        "mob_name": r.choice(["Rattlebone", "Graveclatter","Bonewalker","Dustmarrow","Crypt Rattler"]), 
+        "lootdrops":r.choice(["Bones","Chainmail ""Skeleton head","Goggles"]), 
+        "speed":r.randint(2,3), 
+        "defense":r.randint(1,4),
+        "evasiveness":1,
+        "chance":r.randint(1,5),
+        "debuff": None,
+        "debuffmulti": 1,
+        "moves":["Bone Bash","Arrow"]
+    }
+]
